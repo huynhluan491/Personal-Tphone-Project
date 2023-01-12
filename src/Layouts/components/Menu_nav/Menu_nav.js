92379e9ee8af3b9245956b9e63d1b4d8b4a743b8
huynhluan491 <huynhluan491@gmail.com>
2023-01-12 21:13:03 +0700

    declare

--- src/Layouts/components/Menu_nav/Menu_nav.js
+++ src/Layouts/components/Menu_nav/Menu_nav.js
@@ -1,33 +1,36 @@
-import PropTypes from 'prop-types'
-import classNames from 'classnames/bind'
-import styles from './Menu_nav.module.scss'
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './Menu_nav.module.scss';
 import { NavLink } from 'react-router-dom';
 import MenuItem from './MenuItem';
 import config from '~/config';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
+
+const navItems = [
+    { title: 'Trang chủ', to: config.routes.home },
+    { title: 'Tất cả sản phẩm', to: config.routes.allproducts },
+    { title: 'Điện thoại', to: config.routes.phone },
+    { title: 'Laptop', to: config.routes.laptop },
+    { title: 'Tablet', to: config.routes.tablet },
+    { title: 'Smart watch', to: config.routes.smartclock },
+    { title: 'Thiết bị mạng', to: config.routes.networkdevice },
+    { title: 'Bàn phím', to: config.routes.keyboard },
+    { title: 'Loa', to: config.routes.speaker },
+];
 
 function Menu_Nav() {
-    return (  
+    return (
         <div className={cx('wrapper')}>
             <nav className={cx('nav-item-list')}>
-                <MenuItem title='Trang chủ' to={config.routes.home} />
-                <MenuItem title='Tất cả sản phẩm' to={config.routes.allproducts} />
-                <MenuItem title='Điện thoại' to={config.routes.phone} />
-                <MenuItem title='Laptop' to={config.routes.laptop} />
-                <MenuItem title='Tablet' to={config.routes.tablet} />
-                <MenuItem title='Smart watch' to={config.routes.smartclock} />
-                <MenuItem title='Thiết bị mạng' to={config.routes.networkdevice} />
-                <MenuItem title='Bàn phím' to={config.routes.keyboard} />
-                <MenuItem title='Loa' to={config.routes.speaker} />
+                {navItems.map((item, index) => (
+                    <MenuItem key={index} title={item.title} to={item.to} />
+                ))}
             </nav>
         </div>
     );
 }
 
-
-Menu_Nav.propTypes = {
-
-}
+Menu_Nav.propTypes = {};
 
 export default Menu_Nav;
--- src/Layouts/components/Services/Services.js
+++ src/Layouts/components/Services/Services.js
@@ -1,16 +1,34 @@
-import PropTypes from 'prop-types'
-import classNames from 'classnames/bind'
-import styles from './Services.module.scss'
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './Services.module.scss';
 import ServiceItem from './ServiceItem';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
+
+const services = [
+    {
+        title: 'Ưu đãi',
+        description: 'Với ưu đãi khuyến mãi lên đến 69%.',
+        imgUrl: 'https://tphone-9a576.web.app/static/media/specialoffer.2155ca840f440a4e5ce2.png',
+    },
+    {
+        title: 'Tiết kiệm',
+        description: 'Chúng tôi có chương trình tri ân riêng cho khách hàng.',
+        imgUrl: 'https://tphone-9a576.web.app/static/media/savingmoney.471098c25a761258f14a.png',
+    },
+    {
+        title: 'Tốc độ',
+        description: 'Tốc độ giao hàng chuẩn quốc tế.',
+        imgUrl: 'https://tphone-9a576.web.app/static/media/freeship.d747233115c8c8b73643.png',
+    },
+];
 
 function Services() {
-    return (  
+    return (
         <div className={cx('wrapper')}>
-            <ServiceItem title='Ưu đãi' description='Với ưu đãi khuyến mãi lên đến 69%.' imgUrl='https://tphone-9a576.web.app/static/media/specialoffer.2155ca840f440a4e5ce2.png'/>
-            <ServiceItem title='Tiết kiệm' description='Chúng tôi có chương trình tri ân riêng cho khách hàng.' imgUrl={'https://tphone-9a576.web.app/static/media/savingmoney.471098c25a761258f14a.png'}/>
-            <ServiceItem title='Tốc độ' description='Tốc độ giao hàng chuẩn quốc tế.' imgUrl='https://tphone-9a576.web.app/static/media/freeship.d747233115c8c8b73643.png'/>
+            {services.map((item, index) => (
+                <ServiceItem key={index} title={item.title} description={item.description} imgUrl={item.imgUrl} />
+            ))}
         </div>
     );
 }
