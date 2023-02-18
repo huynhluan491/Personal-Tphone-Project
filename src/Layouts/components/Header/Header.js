39cf54a5e97128ec57154f7c035b71590088b565
huynhluan491 <huynhluan491@gmail.com>
2023-02-18 13:07:48 +0700

    menu sidebar for phone updating

--- src/Layouts/components/Header/Header.js
+++ src/Layouts/components/Header/Header.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import Button from '../../../components/Button';
-import { BackNavigate, CartIcon, MenuIcon, SearchIcon } from '../../../components/Icons';
+import { ArrowDownDrop, BackNavigate, CartIcon, MenuIcon, SearchIcon } from '../../../components/Icons';
 import { Link, useNavigate } from 'react-router-dom';
 import config from '~/config';
 import images from '~/assets/images';
@@ -9,6 +9,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 const cx = classNames.bind(styles);
 
 const navItems = [
@@ -23,10 +24,60 @@ const navItems = [
     { title: 'Loa', to: config.routes.speaker },
 ];
 
+const squadNavBox = [
+    {
+        title: 'Đăng nhập',
+        bg: 'https://cdn.shopify.com/s/files/1/0313/6228/5699/t/32/assets/squad_nav-b-1.svg?v=7085441301639138361642089753',
+        icon: 'https://cdn.shopify.com/s/files/1/0313/6228/5699/t/32/assets/squad_nav-i-1.svg?v=90862635490878400821642089756',
+        route: '/allproduct',
+    },
+    {
+        title: 'Tất cá sản phẩm',
+        bg: 'https://cdn.shopify.com/s/files/1/0313/6228/5699/t/32/assets/squad_nav-b-2.svg?v=133712779800174120111642089754',
+        icon: 'https://cdn.shopify.com/s/files/1/0313/6228/5699/t/32/assets/squad_nav-i-2.svg?v=98400868773840410521642089756',
+        route: '/allproduct',
+    },
+    {
+        title: 'Bộ quà tặng',
+        bg: 'https://cdn.shopify.com/s/files/1/0313/6228/5699/t/32/assets/squad_nav-b-3.svg?v=49745472689912652341642089755',
+        icon: 'https://cdn.shopify.com/s/files/1/0313/6228/5699/t/32/assets/squad_nav-i-3.svg?v=128027689402168929141642089756',
+        route: '/gift',
+    },
+    {
+        title: 'Liên hệ với chúng tôi',
+        bg: 'https://cdn.shopify.com/s/files/1/0313/6228/5699/t/32/assets/squad_nav-b-4.svg?v=59541841797976743711642089755',
+        icon: 'https://cdn.shopify.com/s/files/1/0313/6228/5699/t/32/assets/squad_nav-i-4.svg?v=27299259263485495581642089757',
+        route: '/contact',
+    },
+];
+
 function Header() {
     const { products, totalQuantities } = useSelector((state) => state.CartReducer);
-
+    const [showNavigation, setShowNavigation] = useState(true);
+    const [showMenuIcon, setShowMenuIcon] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
+    const [showCate, setShowCate] = useState(false);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia('(max-width: 767px)');
+
+        const handleMediaQueryChange = () => {
+            if (mediaQuery.matches) {
+                setShowNavigation(false);
+                setShowMenuIcon(true);
+            } else {
+                setShowNavigation(true);
+                setShowMenuIcon(false);
+            }
+        };
+
+        handleMediaQueryChange();
+
+        mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+        return () => mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }, []);
 
     return (
         <div className={cx('wrapper')}>
@@ -34,9 +85,11 @@ function Header() {
                 <button onClick={() => navigate(-1)}>
                     <BackNavigate />
                 </button>
-                <Button whiteNoborder mediumTango>
-                    <MenuIcon />
-                </Button>
+                {showMenuIcon && (
+                    <Button whiteNoborder mediumTango onClick={() => setShowMenu(!showMenu)}>
+                        <MenuIcon />
+                    </Button>
+                )}
                 <Link to={config.routes.home} className={cx('logo-link')}>
                     <img src={images.logo} alt="" />
                 </Link>
@@ -60,15 +113,57 @@ function Header() {
                     </div>
                 </div>
             </div>
-            <div className={cx('navigation-container')}>
-                <nav className={cx('nav-item-list')}>
-                    {navItems.map((item, index) => (
-                        <NavLink key={index} className={(nav) => cx('nav-item', { active: nav.isActive })} to={item.to}>
-                            <span>{item.title}</span>
-                        </NavLink>
-                    ))}
-                </nav>
-            </div>
+            {showMenu && (
+                <div className={cx('sidebar_menu-container')}>
+                    <button whiteNoborder onClick={() => setShowMenu(false)}>
+                        <BackNavigate />
+                    </button>
+                    <ul className={cx('cate-list')}>
+                        <li className={cx('cate-item')} onClick={() => setShowCate(!showCate)}>
+                            <p className={cx('title')}>Danh mục</p>
+                            <ArrowDownDrop />
+                        </li>
+                        {showCate &&
+                            navItems.map((item, index) => (
+                                <Link to={item.to} onClick={() => setShowMenu(false)}>
+                                    <li className={cx('cate-item')}>
+                                        <p className={cx('title')}>{item.title}</p>
+                                    </li>
+                                </Link>
+                            ))}
+                        <li className={cx('cate-item')} onClick={() => setShowCate(!showCate)}>
+                            <p className={cx('title')}>Phần thưởng</p>
+                        </li>
+                        <li className={cx('cate-item')} onClick={() => setShowCate(!showCate)}>
+                            <p className={cx('title')}>Blog</p>
+                        </li>
+                    </ul>
+                    <div className={cx('box-list')}>
+                        {squadNavBox.map((item, index) => (
+                            <Link className={cx('box-item')} key={index} to={item.to}>
+                                <img className={cx('box-bg')} src={item.bg} alt={item.bg} />
+                                <p className={cx('box-title')}>{item.title}</p>
+                                <img className={cx('box-icon')} src={item.icon} alt={item.icon} />
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            )}
+            {showNavigation && (
+                <div className={cx('navigation-container')}>
+                    <nav className={cx('nav-item-list')}>
+                        {navItems.map((item, index) => (
+                            <NavLink
+                                key={index}
+                                className={(nav) => cx('nav-item', { active: nav.isActive })}
+                                to={item.to}
+                            >
+                                <span>{item.title}</span>
+                            </NavLink>
+                        ))}
+                    </nav>
+                </div>
+            )}
         </div>
     );
 }
--- src/components/Icons/Icons.js
+++ src/components/Icons/Icons.js
@@ -428,3 +428,22 @@ export const TickedIcon = ({ width = '2.2rem', height = '2.2rem', className }) =
         ></path>
     </svg>
 );
+
+export const ArrowDownDrop = ({ width = '3rem', height = '3rem', className }) => (
+    <svg
+        className={className}
+        width={width}
+        height={height}
+        stroke="currentColor"
+        fill="currentColor"
+        strokeWidth="0"
+        viewBox="0 0 24 24"
+        class="false"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <g>
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M12 15l-4.243-4.243 1.415-1.414L12 12.172l2.828-2.829 1.415 1.414z"></path>
+        </g>
+    </svg>
+);
