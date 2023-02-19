8e3de35fa97c49f9ba8dcbc782dadea0c293ae84
huynhluan491 <huynhluan491@gmail.com>
2023-02-19 20:39:02 +0700

    searching function updated

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -7,17 +7,58 @@ import Button from '../Button/Button';
 import ProductItem from '../ProductItem/ProductItem';
 import { useEffect } from 'react';
 import images from '~/assets/images';
+import { useDebounce } from '~/hooks';
+import { useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
-const hotProducts = data.slice(20, 44);
 const Search = ({ setSearchToggle }) => {
-    const [productSearched, setProductSearched] = useState(hotProducts);
+    const [productSearched, setProductSearched] = useState([]);
     const [searchValue, setSearchValue] = useState('');
+    const [showLoading, setShowLoading] = useState(false);
+    const debouncedValue = useDebounce(searchValue, 1000);
+    const inputRef = useRef();
 
     useEffect(() => {
-        console.log(hotProducts);
-    });
+        if (!debouncedValue.trim()) {
+            setProductSearched([]);
+            return;
+        }
+        setShowLoading(true);
+
+        setTimeout(() => {
+            const result = data.filter((item) => item.name.toLowerCase().includes(debouncedValue));
+            setProductSearched(result);
+            setShowLoading(false);
+        }, 1000);
+
+        // const fetchingData = async () => {
+        //     const data = await fetch('https://api.npoint.io/2212cc61db4c6711e3b2');
+        //     const dataJson = await data.json();
+        //     const result = await dataJson.filter((item) => item.name.toLowerCase().includes(debouncedValue));
+        //     console.log(result);
+        //     setProductSearched(result);
+        //     setShowLoading(false);
+        // };
+    }, [debouncedValue]);
+
+    const handleClearInput = () => {
+        setSearchValue('');
+        setProductSearched([]);
+        inputRef.current.focus();
+    };
+
+    const handleInputChange = (e) => {
+        const searchValue = e.target.value;
+        const KEY_SPACE = /\s/g;
+
+        if (!KEY_SPACE.test(searchValue[0])) {
+            setSearchValue(searchValue);
+        }
+    };
 
     return (
         <div className={cx('search-container')}>
@@ -26,7 +67,19 @@ const Search = ({ setSearchToggle }) => {
                     <img src={images.logo} alt="logo" />
                 </div>
                 <div className={cx('search-input')}>
-                    <input input="text" placeholder="Nhập tên sản phẩm.." />
+                    <input
+                        type="text"
+                        ref={inputRef}
+                        value={searchValue}
+                        placeholder="Nhập tên sản phẩm.."
+                        onChange={handleInputChange}
+                    />
+                    {!!searchValue && !showLoading > 0 && (
+                        <button className={cx('clear-icon')} onClick={handleClearInput}>
+                            <FontAwesomeIcon icon={faCircleXmark} className={cx('icon')} />
+                        </button>
+                    )}
+                    {showLoading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
                     <Button whiteNoborder className={cx('search-icon')}>
                         <SearchIcon />
                     </Button>
