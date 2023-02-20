92ca1a239945194037eec7eeb94fad0feae804bf
huynhluan491 <huynhluan491@gmail.com>
2023-02-20 20:37:24 +0700

    showToast component update

--- src/function/index.js
+++ src/function/index.js
@@ -1,4 +1,5 @@
 import data from '~/data/data.json';
+import { toast } from 'react-toastify';
 
 export const appleBrandList = data.filter((item) => item.brand === 'apple');
 export const favorite = data.filter((item) => (item.favorite = true));
@@ -10,3 +11,10 @@ export const networkDevice = data.filter((item) => item.category === 'networkdev
 export const smartClock = data.filter((item) => item.category === 'watch');
 export const speaker = data.filter((item) => item.category === 'speaker');
 export const tablet = data.filter((item) => item.category === 'tablet');
+
+export const showToast = () => {
+    toast.success('Thêm vào giỏ hàng thành công!', {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 2000,
+    });
+};
